12bfeaf2c6ef12dd2844b86eff09ba46b57eefd3
Kingerjay <Ogilikingsley96@gmail.com>
2024-12-04 01:51:54 +0100

    router change

--- src/components/home/ServicesSection.jsx
+++ src/components/home/ServicesSection.jsx
@@ -3,9 +3,30 @@ import seaIcon from '/src/assets/sea-icon.svg'
 import warehouseIcon from '/src/assets/warehouse-icon.svg'
 import airIcon from '/src/assets/air-icon.svg'
 import localIcon from '/src/assets/local-icon.svg'
+import { IconBase } from 'react-icons'
 
 
 function ServicesSection() {
+    const services =[
+        {   Icon:seaIcon,
+            title:"Sea Transport Services" , subtitle:"Following the quality of our service thus having gained trust of our many clients."
+        },
+
+        {   Icon:warehouseIcon,
+            title:"Warehousing Services",
+            subtitle:"Following the quality of our service thus having gained trust of our many clients."
+        },
+
+        {   Icon:airIcon,
+            title:"Air Fright Services",
+            subtitle:"Following the quality of our service thus having gained trust of our many clients."
+        },
+
+        {   Icon:localIcon,
+            title:"Local Shipping Services" , subtitle:"Following the quality of our service thus having gained trust of our many clients."
+        }
+        
+    ]
   return (
     <div className ='service-section'>
 
@@ -21,66 +42,24 @@ function ServicesSection() {
 
             {/* service section flex 2 */}
             <div className="service-section-flex2">
-                
-                {/* Sea div */}
-                <div className="sea">
+                {services.map((service,index)=>(
+                    <div key={index}>
 
-                    <div className='sea-icon'>
-                        <img src={seaIcon} alt="" className='service-icons'/>
+                        <div className="sea">
+                          <div className='sea-icon'>
+                       <img src={service.Icon} alt=""  className='service-icons'/>
                     </div>
-                    <div className='sea-text'>
-                        <p className='service-text'>Sea Transport Services</p>
-                        <p className='service-text2'>Following the quality of our service thus having gained trust of our many clients.</p>
-                    </div>                 
+                        
+                        <div className='sea-text'>
+                        <p className='service-text'>{service.title}</p>
+                        <p className='service-text2'>{service.subtitle}</p>
+                        </div>
 
-                </div>
-                {/* End of Sea div */}
+                        </div>
 
-                {/* Warehouse div */}
-                <div className="warehouse">
-
-                    <div className='warehouse-icon'>
-                        <img src={warehouseIcon} alt="" className='service-icons' />
                     </div>
-
-                    <div className='warehouse-text'>
-                        <p className='service-text'>Warehousing Services</p>
-                        <p className='service-text2'>Following the quality of our service thus having gained trust of our many clients.</p>
-                    </div>                 
-
-                </div>
-                {/* End of Warehouse div */}
-
-                {/* Air div */}
-                <div className="air">
-
-                    <div className='air-icon'>
-                        <img src={airIcon} alt="" className='service-icons' />
-                    </div>
-                    
-                    <div className='air-text'>
-                        <p className='service-text'>Air Fright Services</p>
-                        <p className='service-text2'>Following the quality of our service thus having gained trust of our many clients.</p>
-                    </div>                 
-
-                </div>
-                {/* End of Air div */}
-
-                {/* Local div */}
-                <div className="local">
-
-                    <div className='local-icon'>
-                        <img src={localIcon} alt="" className='service-icons' />
-                    </div>
-                    
-                    <div className='local-text'>
-                        <p className='service-text'>Local Shipping Services</p>
-                        <p className='service-text2'>Following the quality of our service thus having gained trust of our many clients.</p>
-                    </div>                 
-
-                </div>
-                {/* End of Local div */}
-
+                ))}
+               
 
             </div>
             {/* End of service section flex 2 */}
